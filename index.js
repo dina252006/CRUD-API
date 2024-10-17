7879fc1fbd41585b7ec65ec8c54914f924fc2794
dina252006 <161189192+dina252006@users.noreply.github.com>
2024-10-18 04:47:37 +0500

    index.js

--- index.js
+++ index.js
@@ -1,35 +1,93 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Product = require('./models/product.model.js');
+const Blog = require('./models/blogs.js'); 
 const app = express();
 
-
+// Middleware
 app.use(express.json());
-
-
-
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-    res.send("Hello from Node Api")
+    res.send("Welcome to the Blogging Platform API");
 });
 
-
-app.post('/api/products', async (req, res) => {
+// Get all blog posts
+app.get('/blogs', async (req, res) => {
     try {
-        const product = await Product.create(req.body);
+        const blogs = await Blog.find({});
+        res.status(200).json(blogs);
     } catch (error) {
-        res.status(500).json({message: error.message});
+        res.status(500).json({ message: error.message });
     }
 });
 
+// Get a single blog post by ID
+app.get('/blogs/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const blog = await Blog.findById(id);
+
+        if (!blog) {
+            return res.status(404).json({ message: 'Blog post not found' });
+        }
+
+        res.status(200).json(blog);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Create a new blog post
+app.post('/blogs', async (req, res) => {
+    try {
+        const blog = await Blog.create(req.body); 
+        res.status(201).json(blog); 
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Update a blog post by ID
+app.put('/blogs/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const blog = await Blog.findByIdAndUpdate(id, req.body, { new: true, runValidators: true });
+
+        if (!blog) {
+            return res.status(404).json({ message: 'Blog post not found' });
+        }
+
+        res.status(200).json(blog);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Delete a blog post by ID
+app.delete('/blogs/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const blog = await Blog.findByIdAndDelete(id);
+
+        if (!blog) {
+            return res.status(404).json({ message: 'Blog post not found' });
+        }
+
+        res.status(200).json({ message: 'Blog post deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
 
 mongoose.connect("mongodb+srv://kozhaevadina15sh:L2oMR8KKKwPydTcO@backenddb.z7dj3.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
-.then(() => {
-    console.log("Connected to database!");
-    app.listen(3000, () => {
-        console.log('Server is running on port 3000');
-    });    
-})
-.catch(() => {
-    console.log("Connection failed!");
-});
+    .then(() => {
+        console.log("Connected to database!");
+        app.listen(3000, () => {
+            console.log('Server is running on port 3000');
+        });
+    })
+    .catch((error) => {
+        console.log("Connection failed!", error);
+    });
